2727c816823d0d304ab0c3a1dce5fb0750de80b4
Pradeep Kumar DV <pradeepkumardv@gmail.com>
2015-06-26 04:00:05 +0530

    updated scripts with latest changes

--- Engg/Design/DB/ERSummary_Procedure.sql
+++ Engg/Design/DB/ERSummary_Procedure.sql
@@ -2,7 +2,8 @@ DROP procedure IF EXISTS `sp_GetERSummary`;
 DELIMITER $$
 CREATE PROCEDURE `sp_GetERSummary`()
 BEGIN
-insert into er_summary(select id, year,episode_disease_category,sum(Total_ER_Visits) as Total_ER_Visits
-from  Medicaid group by episode_disease_category,year);
+insert into er_summary(
+select id, year,UPPER(episode_disease_category) as episode_disease_category,sum(Total_ER_Visits) as Total_ER_Visits
+from  medicaid group by episode_disease_category,year);
 END$$
 DELIMITER ;
