b920dcc1c609fbda23bae65b09993c04e040ed77
Pradeep Kumar DV <pradeepkumardv@gmail.com>
2015-07-04 14:54:32 +0530

    adding DockerFile

---
+++ Engg/Src/REST/RestAPIDocker.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo " ----------------------- dAnalytics REST API Starting ---------------------"
+tar -xvf ./openfda-service.tar
+cd openfda-service/bin
+echo " ------------------- dAnalytics REST Started ------------------------"
